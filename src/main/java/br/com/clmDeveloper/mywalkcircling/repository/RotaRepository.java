4ec45219a387166dcffe45b6fc5bcfc227ace0f1
Cleverson Machado <mxd.maxado@gmail.com>
2018-10-18 21:13:32 -0300

    Server V1.5

--- src/main/java/br/com/clmDeveloper/mywalkcircling/repository/RotaRepository.java
+++ src/main/java/br/com/clmDeveloper/mywalkcircling/repository/RotaRepository.java
@@ -12,10 +12,10 @@ import br.com.clmDeveloper.mywalkcircling.classes.Rota;
 @Repository
 public interface RotaRepository extends JpaRepository<Rota, Long>{
 	
-	@Query("select r from Rota r where r.ID = ?1 and r.proprietario = ?2")
+	@Query("select r from Rota r where r.ID = ?1 and r.email = ?2")
 	Rota findRota(Long ID, String email);
 	
-	@Query("select r from Rota r where r.proprietario = ?1")
+	@Query("select r from Rota r where r.email = ?1")
 	List<Rota> findAllRotas(String email);
 
 }
